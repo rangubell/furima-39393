5bd64bba02db74760922ec9a4d78b782a53be91e
rangubell <rangubell@icloud.com>
2023-06-10 19:05:09 +0900

    コードレビュー後の編集(3)
    ・正規表現の修正
    ・ログインボタンのパス修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 end
