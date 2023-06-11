dd4f1baf6eb3937a6fa3a606c185898211dd54bd
rangubell <rangubell@icloud.com>
2023-06-11 16:16:45 +0900

    バリデーション再設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,9 @@ class Item < ApplicationRecord
   validates :shipping_fee_id,       presence: true
   validates :shipping_origin_id,    presence: true
   validates :shipping_duration_id,  presence: true
-  validates :price,                 presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
   validates :user,                  presence: true
+  validates :image,                 presence: true
 
   has_one_attached :image
 
