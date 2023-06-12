38ded1f43fa243c1002d09bc769b5b0463891927
rangubell <rangubell@icloud.com>
2023-06-12 12:33:03 +0900

    テストコード作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,10 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  before do
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item)
+    @item.user = @user
+  end
+
   describe '商品情報登録' do
     context '商品登録できるとき' do
-      it 'nicknameとemail、passwordとpassword_confirmation、last_nameとfirst_name、last_name_kanaとfirst_name_kanaが存在すれば登録できる' do
+      it 'itemとdescription、category_idとcondition_id、shipping_fee_idとshipping_origin_id、shipping_duration_idとpriceとimageが存在すれば登録できる' do
         expect(@item).to be_valid
+      end
+    end
+    context '商品登録できないとき' do
+      it 'itemが空では登録できない' do
+        @item.item = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item can't be blank")
+      end
+      it 'descriptionが空では登録できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'category_idが空では登録できない' do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'condition_idが空では登録できない' do
+        @item.condition_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'shipping_fee_idが空では登録できない' do
+        @item.shipping_fee_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+      end
+      it 'shipping_origin_idが空では登録できない' do
+        @item.shipping_origin_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping origin can't be blank")
+      end
+      it 'shipping_duration_idが空では登録できない' do
+        @item.shipping_duration_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping duration can't be blank")
+      end
+      it 'priceが空では登録できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが300以下では登録できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it 'priceが9999999以上では登録できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
     end
   end
