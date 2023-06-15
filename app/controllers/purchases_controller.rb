5932d99de38008866e885b362131307fbd08174e
rangubell <rangubell@icloud.com>
2023-06-15 16:19:52 +0900

    リダイレクト先編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,7 +9,7 @@ class PurchasesController < ApplicationController
         @order_form = OrderForm.new
       end
     else
-      redirect_to root_path
+      redirect_to new_user_session_path
     end
   end
 
